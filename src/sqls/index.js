6047a2b1de79e7bda0e779701c355dfe6e5c863d
Owen Luke <hi.owen.luke@gmail.com>
2019-08-09 08:42:01 +0800

    sqls/index.js: add getNamesFromArgs()

--- src/sqls/index.js
+++ src/sqls/index.js
@@ -10,6 +10,35 @@ const getRawSql = (purpose) => {
 	return sqls[purpose];
 };
 
+const getNamesFromArgs = (sql, args) => {
+	let databaseName;
+	let tableName;
+
+	const isNeedDatabaseName = /{databasename}/i.test(sql);
+	const isNeedTableName = /{tablename}/i.test(sql);
+
+	if (isNeedDatabaseName) {
+		databaseName = args.shift(); // The first argument always is database name
+
+		if (isNeedTableName) {
+			tableName = args.shift();
+		}
+		else {
+			// do nothing
+		}
+	}
+	else {
+		if (isNeedTableName) {
+			tableName = args.shift(); // The first argument always is table name
+		}
+		else {
+			// do nothing
+		}
+	}
+
+	return {databaseName, tableName};
+};
+
 /** @name nodber.sqls */
 const fn = (purpose, tablename) => {
 	const dialect = config.dialect;
