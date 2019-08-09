1aca116330912c612a73ea9c2485ec2a1cc63537
Owen Luke <hi.owen.luke@gmail.com>
2019-08-09 08:44:40 +0800

    add src/proxy.js

---
+++ src/proxy.js
@@ -0,0 +1,27 @@
+
+const nodber = require('./');
+const caller = require('caller');
+
+const getPurposeFromCaller = (caller) => {
+	// "/aaa/bbb/cccc.js" => "ccc"
+	return caller.match(/\/([^/]+)?\.js$/i)[1];
+};
+
+/** @name nodber.proxy */
+const fn = async (...args) => {
+	let pathToCaller;
+
+	const lastParam = args[args.length - 1];
+	if (lastParam && lastParam.substr(0, 1) === '/') {
+		pathToCaller = lastParam;
+		args.pop();
+	}
+
+	const purpose = getPurposeFromCaller(pathToCaller || caller());
+	const sql = nodber.sqls(purpose, ...args);
+	const result = await nodber.exec(sql);
+
+	return result;
+};
+
+module.exports = fn;
