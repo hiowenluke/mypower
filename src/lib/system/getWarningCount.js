90d6580be82c8b856c2e4f6b2a0be36b7c0b9c14
Owen Luke <hi.owen.luke@gmail.com>
2019-08-09 15:12:47 +0800

    .

--- src/lib/system/getWarningCount.js
+++ src/lib/system/getWarningCount.js
@@ -2,6 +2,7 @@
 /** @name nodber.getWarningCount */
 const fn = async () => {
 	const result = await global.nodber.proxy();
+	// console.log('WarningCount', result[0].count);
 	return result[0].count;
 };
 
