f7d6b7de1b636a60714602ae7932a07324805af2
Owen Luke <hi.owen.luke@gmail.com>
2019-08-09 15:12:42 +0800

    useDatabase.js: Re-initialize the config of nodber and sequery

--- src/lib/database/useDatabase.js
+++ src/lib/database/useDatabase.js
@@ -1,13 +1,24 @@
 
+const sequery = require('sequelize-raw-query');
+
+const reInitNodber = (databaseName) => {
+	const config = global.nodber.config.get();
+	config.database = databaseName;
+	global.nodber.config.init(config);
+};
+
+const reInitSequery = (databaseName) => {
+	const config = sequery.config.get();
+	config.database = databaseName;
+	sequery.init(config);
+};
+
 /** @name nodber.useDatabase */
 const fn = async (databaseName) => {
 
-	// Re-initialize the config
-	const config = global.nodber.config.get();
-	config.database = databaseName;
-	global.nodber.init(config);
+	reInitNodber();
+	reInitSequery();
 
-	// Change the database connection
 	await global.nodber.proxy(databaseName);
 };
 
