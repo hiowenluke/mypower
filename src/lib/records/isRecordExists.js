1202961f210afb80e0d06a13189a1e7045750025
Owen Luke <hi.owen.luke@gmail.com>
2019-08-09 08:45:42 +0800

    move xxx to src/lib/records

---
+++ src/lib/records/isRecordExists.js
@@ -0,0 +1,10 @@
+
+const nodber = require('../../');
+
+/** @name nodber.isRecordExists */
+const fn = async (tableName, whereStr = '1=1') => {
+	const result = await nodber.count(`select count(*) as count from ${tableName} where ${whereStr}`);
+	return !!result;
+};
+
+module.exports = fn;
