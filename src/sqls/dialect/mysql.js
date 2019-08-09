2ba36382d47e73ce657c747037dfe9ebfdf112df
Owen Luke <hi.owen.luke@gmail.com>
2019-08-09 08:40:55 +0800

    update mysql.js

--- src/sqls/dialect/mysql.js
+++ src/sqls/dialect/mysql.js
@@ -4,11 +4,44 @@
 
 const me = {
 
-	// Because there may be multiple primary keys, sort by ordinal_position
-	getPrimaryKeys: `SELECT column_name as primaryKey FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE table_name= '{tablename}' AND CONSTRAINT_SCHEMA= '{database}' AND constraint_name='PRIMARY' order by ordinal_position`,
+	// -------------------------------------------
+	// Database (schema)
+	// -------------------------------------------
 
-	getTableFieldNames: `select COLUMN_NAME as fieldname from information_schema.COLUMNS where table_name = '{tablename}' and table_schema = '{database}'`,
-	isTableExists: `SELECT table_name FROM information_schema.TABLES WHERE table_name ='{tablename}' and table_schema = '{database}'`,
+	isDatabaseExists: `select schema_name as databasename from information_schema.schemata where schema_name = '{databasename}'`,
+	createDatabase: `create database {databasename} character set gbk`,
+	dropDatabase: `drop database {databasename}`,
+	useDatabase: `use {databasename}`,
+	showDatabases: `show databases`,
+	getSelectedDatabase: `select database() as databasename`,
+
+
+	// -------------------------------------------
+	// Table
+	// -------------------------------------------
+
+	isTableExists: `select table_name as tablename from information_schema.tables where table_name ='{tablename}' and table_schema = '{databasename}'`,
+	createTable: `create table {tablename} ({fields}) {options}`,
+	dropTable: `drop table {tablename}`,
+	truncateTable: `truncate table {tablename}`, // clear table quickly, faster than delete from table
+	descTable: `desc {tablename}`,
+	showTables: `show tables`,
+
+
+	// -------------------------------------------
+	// Fields
+	// -------------------------------------------
+
+	// Because there may be multiple primary keys, sort by ordinal_position
+	getPrimaryKeys: `select column_name as primarykey from information_schema.key_column_usage where table_name= '{tablename}' and constraint_schema= '{databasename}' and constraint_name='primary' order by ordinal_position`,
+	getFieldNames: `select column_name as fieldname from information_schema.columns where table_name = '{tablename}' and table_schema = '{databasename}'`,
+
+
+	// -------------------------------------------
+	// System
+	// -------------------------------------------
+	getWarningCount: `select @@warning_count as count`,
+
 };
 
 module.exports = me;
