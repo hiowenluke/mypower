0688f51d4514c9b758278680bbc5b6c7c7a7234e
Owen Luke <hi.owen.luke@gmail.com>
2019-08-09 08:40:12 +0800

    attach lib.xxx to nodber.xxx

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 
-module.exports.__proto__ = require('kdo')();
+const nodber = require('kdo')();
+
+Object.assign(nodber, nodber.lib.database);
+Object.assign(nodber, nodber.lib.fields);
+Object.assign(nodber, nodber.lib.records);
+Object.assign(nodber, nodber.lib.system);
+Object.assign(nodber, nodber.lib.table);
+Object.assign(nodber, nodber.lib.tools);
+
+global.nodber = nodber;
+module.exports.__proto__ = nodber;
