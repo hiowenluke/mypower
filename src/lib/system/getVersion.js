b0827260d2a9cca5c16d9611bb09371090df3500
Owen Luke <hi.owen.luke@gmail.com>
2019-08-12 08:05:10 +0800

    .

--- src/lib/system/getVersion.js
+++ src/lib/system/getVersion.js
@@ -1,3 +1,10 @@
 
+const nodber = require('../../');
+
 /** @name nodber.getVersion */
-module.exports = require('../createDoFn')();
+const fn = async () => {
+	const result = await nodber.proxy();
+	return result[0].version;
+};
+
+module.exports = fn;
