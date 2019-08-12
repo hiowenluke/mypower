95d060920ce129e28216bb52c7418f465d6850ec
Owen Luke <hi.owen.luke@gmail.com>
2019-08-12 08:41:25 +0800

    .

---
+++ src/lib/table/dropTables.js
@@ -0,0 +1,19 @@
+
+const nodber = require('../../');
+
+/** @name nodber.dropTables */
+const fn = async (tableNames) => {
+	if (typeof tableNames === 'string') {
+		tableNames = [tableNames];
+	}
+
+	let result;
+	for (let i = 0; i < tableNames.length; i ++) {
+		const tableName = tableNames[i];
+		result = await nodber.dropTable(tableName);
+	}
+
+	return result;
+};
+
+module.exports = fn;
