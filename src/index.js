7195d8e235d02d2b9be7055d8dc81bff72d75bac
Owen Luke <hi.owen.luke@gmail.com>
2019-08-13 13:57:46 +0800

    attach crud/* to nodber

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 const nodber = require('kdo')();
+const sequery = require('sequelize-raw-query');
 
+Object.assign(nodber, nodber.crud);
 Object.assign(nodber, nodber.lib.database);
 Object.assign(nodber, nodber.lib.fields);
 Object.assign(nodber, nodber.lib.records);
 Object.assign(nodber, nodber.lib.system);
 Object.assign(nodber, nodber.lib.table);
 Object.assign(nodber, nodber.lib.tools);
+
+nodber.sequelizeOp = sequery.Sequelize.Op;
 
 global.nodber = nodber;
 module.exports.__proto__ = nodber;
