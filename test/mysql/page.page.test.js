25ddb6f5fd0bd1a2efeeb74592045c32995590c6
Owen Luke <hi.owen.luke@gmail.com>
2019-08-14 18:52:10 +0800

    .

--- test/mysql/page.page.test.js
+++ test/mysql/page.page.test.js
@@ -29,4 +29,34 @@ describe('MySQL - page/page', () => {
 		expect(result.pageSize === 3 && result.count === 7).to.be.true;
 	});
 
+	it(`.page({table, fields, order, pageNumber, pageSize})`, async () => {
+		const fields = 'username';
+		const order = 'id';
+		const pageNumber = 2;
+		const pageSize = 3;
+		const result = await nodber.page({table, fields, order, pageNumber, pageSize});
+		expect(result.pageSize === 3 && result.count === 7 && Object.keys(result.rows[0]).length === 1).to.be.true;
+	});
+
+	it(`.page({table, fields, where, order, pageNumber, pageSize})`, async () => {
+		const fields = 'id, username';
+		const where = 'id > 2';
+		const order = 'id';
+		const pageNumber = 2;
+		const pageSize = 3;
+		const result = await nodber.page({table, fields, where, order, pageNumber, pageSize});
+		expect(result.pageSize === 3 && result.count === 5 && Object.keys(result.rows[0]).length === 2).to.be.true;
+	});
+
+	it(`.page({table, fields, where, order, pageNumber, pageSize, data})`, async () => {
+		const fields = 'id, username';
+		const where = 'isaverangers = :isaverangers';
+		const order = 'id';
+		const pageNumber = 2;
+		const pageSize = 3;
+		const data = {isaverangers: 1};
+		const result = await nodber.page({table, fields, where, order, pageNumber, pageSize, data});
+		expect(result.pageSize === 3 && result.count === 6 && Object.keys(result.rows[0]).length === 2).to.be.true;
+	});
+
 });
