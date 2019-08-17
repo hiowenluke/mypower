9ae52ef6452537e4a5a39e6ace7d2f9bc44ad9dd
Owen Luke <hi.owen.luke@gmail.com>
2019-08-17 17:00:45 +0800

    updateAndInsert.js: Replace ":xxx" with data.xxx for unicode xxx

--- src/crud/__lib/updateAndInsert.js
+++ src/crud/__lib/updateAndInsert.js
@@ -12,8 +12,13 @@ const fn = async (sqlTemplate, table, data) => {
 	const tableName = table;
 	const {nameParams, valueParams, setParams} = await lib.fieldParams.genAllByTableName(tableName);
 
-	const sql = utils.sqlTemplate(sqlTemplate, {tableName, nameParams, valueParams, setParams});
-	const result = await nodber.exec(sql, {replacements: data});
+	let sql = utils.sqlTemplate(sqlTemplate, {tableName, nameParams, valueParams, setParams});
+
+	// Replace ":xxx" with data.xxx for unicode xxx,
+	// otherwise, sequelize will occur an error if the xxx included unicode character.
+	sql = utils.sqlReplacement(sql, data);
+
+	const result = await nodber.exec(sql);
 	return result;
 };
 
