2010783b9edcb8c84d1aaad4d2b17a89ece0d1be
Owen Luke <hi.owen.luke@gmail.com>
2019-08-17 17:11:26 +0800

    .

--- src/__utils/sqlReplacement.js
+++ src/__utils/sqlReplacement.js
@@ -1,10 +1,13 @@
 
 /** @name utils.sqlReplacement */
 const fn = (sql, data) => {
+	if (!data) return sql;
+
 	Object.keys(data).forEach(key => {
 		const reg = new RegExp(':\\b' + key + '\\b', 'g');
 		sql = sql.replace(reg, '"' + data[key].replace('"', '\\"') + '"');
 	});
+
 	return sql;
 };
 
