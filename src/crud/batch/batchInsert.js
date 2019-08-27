39cf85d92f45d493bdfbceae55a9d5602db0ce9d
Owen Luke <hi.owen.luke@gmail.com>
2019-08-27 14:52:30 +0800

    add nodber.batchInsert()

---
+++ src/crud/batch/batchInsert.js
@@ -0,0 +1,19 @@
+
+const nodber = require('../..');
+
+const fn = async (tableName, dataArr) => {
+
+	if (!Array.isArray(dataArr)) {
+		dataArr = [dataArr];
+	}
+
+	let result;
+	for (let i = 0; i < dataArr.length; i ++) {
+		const data = dataArr[i];
+		result = await nodber.insert(tableName, data);
+	}
+
+	return result;
+};
+
+module.exports = fn;
