9724672163ccc1ff88d0a348e2b51e1572c0ec9f
Owen Luke <hi.owen.luke@gmail.com>
2019-08-27 14:52:20 +0800

    add nodber.batchDelete()

---
+++ src/crud/batch/batchDelete.js
@@ -0,0 +1,19 @@
+
+const nodber = require('../..');
+
+const fn = async (tableName, whereArr) => {
+
+	if (!Array.isArray(whereArr)) {
+		whereArr = [whereArr];
+	}
+
+	let result;
+	for (let i = 0; i < whereArr.length; i ++) {
+		const where = whereArr[i];
+		result = await nodber.delete(tableName, where);
+	}
+
+	return result;
+};
+
+module.exports = fn;
