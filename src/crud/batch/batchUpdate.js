e0389f47446a75c96f4a03c6b3cbaecf51f04feb
Owen Luke <hi.owen.luke@gmail.com>
2019-08-27 14:52:44 +0800

    add nodber.batchUpdate()

---
+++ src/crud/batch/batchUpdate.js
@@ -0,0 +1,21 @@
+
+const nodber = require('../..');
+
+const fn = async (tableName, dataArr, whereArr) => {
+
+	if (!Array.isArray(dataArr)) {
+		dataArr = [dataArr];
+		whereArr = [whereArr];
+	}
+
+	let result;
+	for (let i = 0; i < dataArr.length; i ++) {
+		const data = dataArr[i];
+		const where = whereArr[i];
+		result = await nodber.update(tableName, data, where);
+	}
+
+	return result;
+};
+
+module.exports = fn;
