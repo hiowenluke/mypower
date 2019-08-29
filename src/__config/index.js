270c5238509780ec27ba3ee58bde57fc4a0988be
Owen Luke <hi.owen.luke@gmail.com>
2019-08-29 14:36:36 +0800

    .

--- src/__config/index.js
+++ src/__config/index.js
@@ -8,6 +8,12 @@ const me = {
 	host: '127.0.0.1',
 	port: 3306,
 
+	// If it is true, save the data to global for sequelize-raw-query.
+	// 		If the user project includes multiple subprojects, it is needed to enable global mode.
+	// 		Otherwise, since the sequelize in each subproject is a different instance
+	// 		and cannot share the same data, it will cause an error.
+	enableGlobal: false,
+
 	init(...args) {
 		this.set(...args);
 	},
