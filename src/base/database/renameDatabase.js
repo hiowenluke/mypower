b3dfde5022ad543b2bd2a49ca4114835b6b497bc
Owen Luke <hi.owen.luke@gmail.com>
2019-09-11 10:51:38 +0800

    .

--- src/base/database/renameDatabase.js
+++ src/base/database/renameDatabase.js
@@ -3,14 +3,22 @@ const shell = require('shelljs');
 const config = require('../../__config');
 
 /** @name nodber.renameDatabase */
-const fn = async (oldName, newName) => {
+const fn = async (oldDatabaseName, newDatabaseName) => {
+
+	if (await nodber.isDatabaseExists(newDatabaseName)) {
+		return false;
+	}
 
 	const {username, password} = config;
+
 	shell.exec(`
-		mysql -u${username} -p${password} -e "create database if not exists ${newName}" && 
-		list_table=$(mysql -u${username} -p${password} -Nse "select table_name from information_schema.TABLES where TABLE_SCHEMA='${oldName}'") && 
-		for table in $list_table; do mysql -u${username} -p${password} -e "rename table ${oldName}.$table to ${newName}.$table"; done
+		mysql -u${username} -p${password} -e "create database if not exists ${newDatabaseName}" && 
+		list_table=$(mysql -u${username} -p${password} -Nse "select table_name from information_schema.TABLES where TABLE_SCHEMA='${oldDatabaseName}'") && 
+		for table in $list_table; do mysql -u${username} -p${password} -e "rename table ${oldDatabaseName}.$table to ${newDatabaseName}.$table"; done &&
+		mysql -u${username} -p${password} -e "drop database ${oldDatabaseName}"		
 	`, {silent: true});
+
+	return true;
 };
 
 module.exports = fn;
