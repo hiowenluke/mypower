8d4e1fa25fa61f2c60432bd452b1d9fe1359e913
Owen Luke <hi.owen.luke@gmail.com>
2019-09-20 18:51:49 +0800

    .

--- src/base/record/recordsCount.js
+++ src/base/record/recordsCount.js
@@ -1,8 +1,13 @@
 
 const nodber = require('../..');
+const sequery = require('sequelize-raw-query');
 
 /** @name nodber.recordsCount */
 const fn = async (tableName, whereStr) => {
+	if (typeof whereStr === 'object') {
+		whereStr = sequery.getWhereConditions(whereStr);
+	}
+
 	return await nodber.count(tableName, whereStr);
 };
 
