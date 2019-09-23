1c491bd9445b913a62540bd32e7c86ba5fb3cb76
Owen Luke <hi.owen.luke@gmail.com>
2019-09-23 16:50:09 +0800

    .

--- src/init.js
+++ src/init.js
@@ -1,11 +1,13 @@
 
 const sequery = require('sequelize-raw-query');
+const myCli = require('mysql-cli-exec');
 const config = require('./__config');
 
 /** @name nodber.init */
 const fn = (cfg) => {
 	config.init(cfg);
 	sequery.init(cfg);
+	myCli.init(cfg);
 };
 
 module.exports = fn;
