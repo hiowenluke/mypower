d157245f2c62e47c59d49ba935d1575470a7d2cf
Owen Luke <hi.owen.luke@gmail.com>
2019-09-24 16:40:18 +0800

    .

--- src/base/table/truncateTable.js
+++ src/base/table/truncateTable.js
@@ -1,3 +1,10 @@
 
+const my = require('../..');
+
 /** @name my.truncateTable */
-module.exports = require('../createDoFn')();
+const fn = async (tableName, databaseName) => {
+	const result = await my.proxy(databaseName, tableName);
+	return result.warningStatus === 0;
+};
+
+module.exports = fn;
