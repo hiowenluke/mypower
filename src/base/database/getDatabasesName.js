2609f607dfc4e1b3e96e7fa3f2ef055d1428026b
Owen Luke <hi.owen.luke@gmail.com>
2019-10-22 14:10:33 +0800

    Update getDatabasesName.js

--- src/base/database/getDatabasesName.js
+++ src/base/database/getDatabasesName.js
@@ -1,6 +1,8 @@
 
 // The alias of my.showDatabases
 
+const my = require('../..');
+
 /** @name my.getDatabasesName */
 const fn = async (serverConfig) => {
 	return await my.showDatabases(serverConfig);
