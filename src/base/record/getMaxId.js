011c4d90ed18de086a3d8307fb8cf5d47476d21b
Owen Luke <hi.owen.luke@gmail.com>
2019-11-12 16:15:21 +0800

    Update getMaxId.js
    If the argument idName is where

--- src/base/record/getMaxId.js
+++ src/base/record/getMaxId.js
@@ -2,8 +2,21 @@
 const my = require('../..');
 
 /** @name my.getMaxId */
-const fn = async (tableName, idName = 'id') => {
-	return await my.getMaxFieldValue(tableName, idName);
+const fn = async (tableName, idName, where) => {
+
+	if (!idName) {
+		idName = 'id';
+	}
+
+	else
+
+	// If the argument idName is where
+	if (typeof idName === 'object' || typeof idName === 'string' && idName.indexOf(' ') >= 0) {
+		where = idName;
+		idName = 'id';
+	}
+
+	return await my.getMaxFieldValue(tableName, idName, where);
 };
 
 module.exports = fn;
