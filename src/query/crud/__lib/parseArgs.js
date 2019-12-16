897a99948ad314f8df608a815781175d76cc2c7d
Owen Luke <hi.owen.luke@gmail.com>
2019-12-16 16:36:39 +0800

    Update parseArgs.js

--- src/query/crud/__lib/parseArgs.js
+++ src/query/crud/__lib/parseArgs.js
@@ -18,9 +18,9 @@ const fixArgs = require('./fixArgs');
 const fn = (...args) => {
 	let table, fields, where;
 
-	// Remove undefined elements from args
-	// [tableName, undefined] => [tableName]
-	while (args[args.length - 1] === undefined) {
+	// Remove the empty (undefined or null) arguments at the ending
+	// [tableName, undefined, null] => [tableName]
+	while (args[args.length - 1] === undefined || args[args.length - 1] === null) {
 		args.pop();
 	}
 
@@ -29,16 +29,16 @@ const fn = (...args) => {
 	}
 
 	if (args.length === 1) {
-		const arg = args[0];
+		const a0 = args[0];
 
 		// ({table, fields, where})
-		if (typeof arg === "object") {
-			({table, fields, where} = arg);
+		if (a0 && typeof a0 === "object") {
+			({table, fields, where} = a0);
 		}
 		else {
 			// (table)
-			if (typeof arg === 'string') {
-				table = arg;
+			if (typeof a0 === 'string') {
+				table = a0;
 			}
 			else {
 				throw new Error("Wrong argument");
@@ -51,29 +51,29 @@ const fn = (...args) => {
 		if (args.length === 2) {
 			table = args[0];
 
-			let arg = args[1];
+			let a1 = args[1];
 			let isfields;
 
 			// (table, {id: 1})
-			if (_.isPlainObject(arg)) {
+			if (_.isPlainObject(a1)) {
 				isfields = false;
 			}
 			else {
 				// (table, ['f1', 'f2', ...])
-				if (Array.isArray(arg)) {
+				if (Array.isArray(a1)) {
 					isfields = true;
 				}
 				else {
-					if (typeof arg === 'string') {
+					if (typeof a1 === 'string') {
 
 						// (table, 'f_1')
-						if (/^\w+$/.test(arg)) {
+						if (/^\w+$/.test(a1)) {
 							isfields = true;
 						}
 						else {
 
 							// (table, 'f1, f2')
-							if (arg.indexOf(',') >= 0 && arg.indexOf('(') === -1) {
+							if (a1.indexOf(',') >= 0 && a1.indexOf('(') === -1) {
 								isfields = true;
 							}
 							else {
@@ -90,10 +90,10 @@ const fn = (...args) => {
 			}
 
 			if (isfields) {
-				fields = arg;
+				fields = a1;
 			}
 			else {
-				where = arg;
+				where = a1;
 			}
 		}
 		else {
@@ -106,7 +106,7 @@ const fn = (...args) => {
 				where = args[2];
 			}
 			else {
-				throw new Error('Too many arguments. Please use {table, fields, where, ... }.');
+				throw new Error('Too many arguments. Please use ({table, fields, where, ... }) instead of (table, fields, where, ...).');
 			}
 		}
 	}
