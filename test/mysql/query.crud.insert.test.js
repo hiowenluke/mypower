e24864c40c374fa7c66697d1561c57701259673b
Owen Luke <hi.owen.luke@gmail.com>
2020-01-27 01:39:29 +0800

    update tests for my.insert

--- test/mysql/query.crud.insert.test.js
+++ test/mysql/query.crud.insert.test.js
@@ -12,23 +12,45 @@ describe('MySQL - query/crud/insert', () => {
 	my.init(config);
 	prepareForTesting();
 
-	it(`.insert(table, data) // data = {username: 'haha'}`, async () => {
-		const data = {username: 'haha'};
-		const result = await my.insert(table, data);
-		expect(typeof result === "number").to.be.true;
+	it(`.insert(table, data) // data = {username: 'jerry'}`, async () => {
+		const maxId = await my.getMaxId(table);
+
+		const data = {username: 'jerry'};
+		await my.insert(table, data);
+
+		const newMaxId = await my.getMaxId(table);
+		expect(newMaxId - maxId === 1).to.be.true;
+	});
+
+	it(`.insert(table, data, isReturnNewId) // data = {username: 'tom'}, isReturnNewId = true`, async () => {
+		const maxId = await my.getMaxId(table);
+
+		const data = {username: 'tom'};
+		const isReturnNewId = true;
+		const newMaxId = await my.insert(table, data, isReturnNewId);
+
+		expect(newMaxId - maxId === 1).to.be.true;
 	});
 
 	// for Unicode
 	it(`.insert(table, data) // data = {username: '美国队长'}`, async () => {
+		const maxId = await my.getMaxId(table);
+
 		const data = {username: '美国队长'};
-		const result = await my.insert(table, data);
-		expect(typeof result === "number").to.be.true;
+		await my.insert(table, data);
+
+		const newMaxId = await my.getMaxId(table);
+		expect(newMaxId - maxId === 1).to.be.true;
 	});
 
 	it(`.insert(table, data) // data = {username: 'hi', memo: 'included " here'}`, async () => {
+		const maxId = await my.getMaxId(table);
+
 		const data = {username: 'hi', memo: 'included " here'};
-		const result = await my.insert(table, data);
-		expect(typeof result === "number").to.be.true;
+		await my.insert(table, data);
+
+		const newMaxId = await my.getMaxId(table);
+		expect(newMaxId - maxId === 1).to.be.true;
 	});
 
 });
